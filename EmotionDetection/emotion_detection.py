71b3c7b39825784e84b7812086bdfa14a8c43bfc
ramezyoussef <ramez_youssef123@outlook.com>
2025-08-07 10:03:52 -0400

    f

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -4,6 +4,9 @@ def emotion_detector(text_to_analyze):
     url='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header={"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj={ "raw_document": { "text": text_to_analyze }}
+
+
+    
     response = requests.post(url, json = myobj, headers=header)
     formatted_response =json.loads(response.text)
 
@@ -16,8 +19,11 @@ def emotion_detector(text_to_analyze):
     sadness=emotions.get('sadness',0)
     dominant_emotion=max(emotions,key=emotions.get)
     
+    if response.status_code == 200:
+        return {'anger':anger,'disgust':disgust,'fear':fear,'joy':joy,'sadness':sadness,'dominant_emotion':dominant_emotion}
+    elif response.status_code == 400:
+        return {'anger':None,'disgust':None,'fear':None,'joy':None,'sadness':None,'dominant_emotion':None}
     
-    return {'anger':anger,'disgust':disgust,'fear':fear,'joy':joy,'sadness':sadness,'dominant_emotion':dominant_emotion}
 
 
 
---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template, jsonify
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_detector():
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    if not text_to_analyze or text_to_analyze.strip() == "":
+        return jsonify({'error': 'Invalid text! Please try again!'}), 200
+
+    response = emotion_detector(text_to_analyze)
+
+    if response['dominant_emotion'] is None:
+        return jsonify({'error': 'Invalid text! Please try again!'}), 200
+
+    return jsonify(response)
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000)
